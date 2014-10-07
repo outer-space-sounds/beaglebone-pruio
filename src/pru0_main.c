23781f8a73782769c6edf9f6ed61f299c5a74a76
Rafael Vega <email.rafa@gmail.com>
2014-10-07 17:27:07 +0000

    First stab at syncing mux control pins.

--- src/pru0_main.c
+++ src/pru0_main.c
@@ -13,6 +13,7 @@
 #define CONTROL_MODULE 0x44e10000
 #define CONF_P9_11 0x870
 #define CONF_P9_12 0x878
+#define CONF_P9_13 0x874
 
 #define GPIO0 0x44e07000
 #define GPIO1 0x4804c000
@@ -25,7 +26,11 @@
 /////////////////////////////////////////////////////////////////////
 // GLOBALS
 //
-/* void init_adc(); */
+void init_ocp();
+void init_adc();
+void init_gpio();
+inline void set_mux_control(unsigned int ctl);
+
 /* volatile unsigned int* shared_ram; */
 volatile register unsigned int __R31;
 
@@ -33,10 +38,91 @@ volatile register unsigned int __R31;
 // MAIN
 //
 int main(int argc, const char *argv[]){
+   init_ocp();
+   init_gpio();
+   init_adc();
+
+   unsigned int i;
+   unsigned int mux_control = 7;
+   while(1){
+      mux_control>6 ? mux_control=0 : mux_control++;
+      set_mux_control(mux_control);
+
+      for(i=0; i<5000; i++); // Approx. 125uS = 1ms/8
+   }
+
+   /* __R31 = 35; */
+   /* __halt(); */
+
+   return 0;
+}
+
+inline void set_mux_control(unsigned int ctl){
+   // 3 bits for mux control: 
+   // [P9_11 GPIO0[30], P9_12 GPIO1[28], P9_13 GPIO0[31]]
+   switch(ctl){
+      case 0:
+         HWREG(GPIO0 + GPIO_DATAOUT) &= ~(1<<30);
+         HWREG(GPIO1 + GPIO_DATAOUT) &= ~(1<<28);
+         HWREG(GPIO0 + GPIO_DATAOUT) &= ~(1<<31);
+         break;
+
+      case 1:
+         HWREG(GPIO0 + GPIO_DATAOUT) &= ~(1<<30);
+         HWREG(GPIO1 + GPIO_DATAOUT) &= ~(1<<28);
+         HWREG(GPIO0 + GPIO_DATAOUT) |= ((unsigned int)1)<<31; // Explicit cast to get rig of compiler warning
+         break;
+
+      case 2:
+         HWREG(GPIO0 + GPIO_DATAOUT) &= ~(1<<30);
+         HWREG(GPIO1 + GPIO_DATAOUT) |= 1<<28;
+         HWREG(GPIO0 + GPIO_DATAOUT) &= ~(1<<31);
+         break;
+
+      case 3:
+         HWREG(GPIO0 + GPIO_DATAOUT) &= ~(1<<30);
+         HWREG(GPIO1 + GPIO_DATAOUT) |= 1<<28;
+         HWREG(GPIO0 + GPIO_DATAOUT) |= ((unsigned int)1)<<31;
+         break;
+
+      case 4:
+         HWREG(GPIO0 + GPIO_DATAOUT) |= 1<<30;
+         HWREG(GPIO1 + GPIO_DATAOUT) &= ~(1<<28);
+         HWREG(GPIO0 + GPIO_DATAOUT) &= ~(1<<31);
+         break;
+
+      case 5:
+         HWREG(GPIO0 + GPIO_DATAOUT) |= 1<<30;
+         HWREG(GPIO1 + GPIO_DATAOUT) &= ~(1<<28);
+         HWREG(GPIO0 + GPIO_DATAOUT) |= ((unsigned int)1)<<31;
+         break;
+
+      case 6:
+         HWREG(GPIO0 + GPIO_DATAOUT) |= 1<<30;
+         HWREG(GPIO1 + GPIO_DATAOUT) |= 1<<28;
+         HWREG(GPIO0 + GPIO_DATAOUT) &= ~(1<<31);
+         break;
+
+      default: //7
+         HWREG(GPIO0 + GPIO_DATAOUT) |= 1<<30;
+         HWREG(GPIO1 + GPIO_DATAOUT) |= 1<<28;
+         HWREG(GPIO0 + GPIO_DATAOUT) |= ((unsigned int)1)<<31;
+         break;
+   }
+
+}
+
+/////////////////////////////////////////////////////////////////////
+// INIT ADC
+//
+
+void init_ocp(){
    // Enable OCP so we can access the whole memory map for the
    // device from the PRU. Clear bit 4 of SYSCFG register
    HWREG(SYSCFG) &= ~(1 << 4);
+}
 
+void init_gpio(){
    // Enable GPIO Module. GPIO_CTRL Register
    /* HWREG(GPIO0 + GPIO_CTRL) = 0x00; */
    /* HWREG(GPIO1 + GPIO_CTRL) )= 0x00; */
@@ -52,29 +138,14 @@ int main(int argc, const char *argv[]){
 
    // P9_12 pin as output, GPIO1[28], no pull.
    HWREG(CONTROL_MODULE + CONF_P9_12) = 0x0F;
+   HWREG(GPIO1 + GPIO_OE) &= ~(1<<28);
 
-
-   int i;
-   while(1){
-      for(i=0; i<100; i++);
-      HWREG(GPIO0 + GPIO_SETDATAOUT) = 1<<30;
-      HWREG(GPIO1 + GPIO_SETDATAOUT) = 1<<28;
-
-      for(i=0; i<100; i++);
-      HWREG(GPIO0 + GPIO_CLEARDATAOUT) = 1<<30;
-      HWREG(GPIO1 + GPIO_CLEARDATAOUT) = 1<<28;
-   }
-
-   /* __R31 = 35; */
-   /* __halt(); */
-
-   return 0;
+   // P9_13 pin as output, GPIO0[31], no pull.
+   HWREG(CONTROL_MODULE + CONF_P9_13) = 0x0F;
+   HWREG(GPIO0 + GPIO_OE) &= ~(1<<31);
 }
 
-/////////////////////////////////////////////////////////////////////
-// INIT ADC
-//
-/* void init_adc(){ */
+void init_adc(){
 /*    // Enable OCP so we can access the whole memory map for the */
 /*    // device from the PRU. Clear bit 4 of SYSCFG register */
 /*    HWREG(0x26004) &= 0xFFFFFFEF; */
@@ -189,4 +260,4 @@ int main(int argc, const char *argv[]){
 /*  */
 /*    // Enable Module (start sampling). ADC_CTRL register */
 /*    HWREG(0x44e0d040) |= 0x01; */
-/* } */
+}
